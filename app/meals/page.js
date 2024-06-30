572e0985276e439dc56b1def9420aea368816f39
Nuraib <nuraibaa@code.edu.az>
2024-06-30 22:41:01 +0400

    MealsPage updated & MealsItem, MealsGrid created

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import Link from "next/link";
+import styles from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
 
 export default function MealsPage() {
   return (
-    <div>page</div>
-  )
+    <>
+      <header className={styles.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={styles.highligt}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={styles.cta}>
+          <Link href="/meals/share">
+            Share Your Favortie Recipe
+          </Link>
+        </p>
+      </header>
+      <main className={styles.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
 }
